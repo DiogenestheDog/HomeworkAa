4345598f8cafcc74f886e32686094f28c6329cd9
DiogenestheDog <rguthrie14@gmail.com>
2019-07-14 22:44:49 -0700

    implemented bfs

--- W4D1/homework/graph_node.rb
+++ W4D1/homework/graph_node.rb
@@ -1,7 +1,28 @@
 class GraphNode
 
+    attr_reader :val
+    attr_accessor :neighbors
+
     def initialize(val, neighbors =nil)
         @val = val
         @neighbors ||= []
     end
+
 end
+
+    def bfs(starting_node, target_value)
+        # make set dont move to neighbor if val in set
+        # assuming all unique vals
+        checked = Set.new
+        to_check = []
+        to_check << starting_node
+        while to_check.length > 0
+            temp = to_check.shift
+            return temp.val if temp.val == target_value
+            checked << temp
+            temp.neighbors.each do |node|
+                to_check.push(node) unless checked.include?(node)
+            end           
+        end
+        nil 
+    end
